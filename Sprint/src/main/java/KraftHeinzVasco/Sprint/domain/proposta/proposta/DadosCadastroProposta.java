e13d32f338f19577f822b00a858fa071d80b6bfe
pdr0-42 <pviniciuspsilva@gmail.com>
2023-10-01 14:03:00 -0300

    Adicionando frontend ao projeto

--- Sprint/src/main/java/KraftHeinzVasco/Sprint/domain/proposta/proposta/DadosCadastroProposta.java
+++ Sprint/src/main/java/KraftHeinzVasco/Sprint/domain/proposta/proposta/DadosCadastroProposta.java
@@ -1,18 +1,14 @@
 package KraftHeinzVasco.Sprint.domain.proposta.proposta;
 
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosCadastroProposta(
 
-         String nomeusuario,
-         @NotNull
-         Estado estado,
-         @NotNull
-         Tipoesg tipoesg,
-         @NotBlank
-         String mensagemproposta
+        String nomeusuario,
+        @NotNull Estado estado,
+        @NotNull Tipoesg tipoesg,
+        @NotBlank String mensagemproposta
 
-
-) { }
+) {
+}
